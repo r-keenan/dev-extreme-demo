0ec644591fbbeaef5eeda8a9db3636b04a3b005a
Ross Keenan <RKeenan@onestaffmedical.com>
2022-03-02 08:40:58 -0600

    moved data to a json file to clean up the datagrid component.

--- src/components/DataGrid.jsx
+++ src/components/DataGrid.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import data from "./data/muiData.json";
 //import { useDemoData } from "@mui/x-data-grid-generator";
 
 export default function MuiDataGrid() {
@@ -8,72 +9,6 @@ export default function MuiDataGrid() {
   //  rowLength: 100,
   //  maxColumns: 6,
   //});
-
-  const columns = [
-    {
-      field: "empId",
-      headerName: "Employee Id",
-      type: "number",
-      flex: 1,
-    },
-    {
-      field: "firstName",
-      headerName: "First Name",
-      flex: 1,
-      editable: true,
-    },
-    {
-      field: "lastName",
-      headerName: "Last Name",
-      flex: 1,
-      editable: true,
-    },
-    {
-      field: "issue",
-      headerName: "Issue",
-      flex: 2,
-      editable: true,
-    },
-    {
-      field: "filedBy",
-      headerName: "Filed By",
-      flex: 1,
-      editable: true,
-    },
-    {
-      field: "actionTaken",
-      headerName: "Action Taken",
-      sortable: true,
-      flex: 2,
-    },
-  ];
-
-  const rows = [
-    {
-      empId: 666,
-      lastName: "Case",
-      firstName: "Krystal",
-      issue: "Mean to new hire",
-      filedBy: "Ross Keenan",
-      actionTaken: "Moved her to another building",
-    },
-    {
-      empId: 1,
-      lastName: "Debbie",
-      firstName: "Heller",
-      issue: "Drinking on the job",
-      filedBy: "Kyle Wickersham",
-      actionTaken: "Mandatory AA meetings",
-    },
-    {
-      empId: 2,
-      lastName: "Tyrone",
-      firstName: "Biggums",
-      issue: "Under the influence of drugs while at work",
-      filedBy: "Self",
-      actionTaken: "Mandatory NA Meetings",
-    },
-  ];
 
   const [sortModel, setSortModel] = useState([
     {
@@ -85,8 +20,8 @@ export default function MuiDataGrid() {
   return (
     <div style={{ height: 400, width: "100%" }}>
       <DataGrid
-        rows={rows}
-        columns={columns}
+        rows={data.rows}
+        columns={data.columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
